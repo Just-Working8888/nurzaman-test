f5642eb137c85ff4b201b7aa10f6af587fa163fe
Bekbolot <justworking8888@gmail.com>
2023-12-18 17:18:17 +0600

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -419,20 +419,7 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     function smoothScroll(e) {
-        e.preventDefault();
-
-        const targetId = this.getAttribute('href').substring(1);
-        const targetElement = document.getElementById(targetId);
-
-        if (targetElement) {
-            targetElement.scrollIntoView({
-                behavior: 'smooth',
-                block: 'start'
-            });
             menu.style.left = '-100%'
-        } else {
-            console.error(`Element with id ${targetId} not found`);
-        }
     }
 });
 
